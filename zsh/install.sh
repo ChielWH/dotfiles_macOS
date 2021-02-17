141325344383a757897e86eff6b75d770cba8c8c
Chiel Heldoorn <chiel_heldoorn@hotmail.com>
2021-02-17 16:04:02 +0100

    Added more aliasses and blurred explicit info

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,8 +1,12 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"/.
 
-scp ./zsh/my.zsh-theme ~/.oh-my-zsh/themes/my.zsh-theme
+# Useful variables and aliasses
 scp ./zsh/.variables ~/.variables 
 scp ./zsh/.aliasses ~/.aliasses 
+source ~/.aliasses 
+
+# Actual ZSH set-up
+scp ./zsh/my.zsh-theme ~/.oh-my-zsh/themes/my.zsh-theme
 scp ./zsh/.zshrc ~/.zshrc 
 source ~/.zshrc 
 
